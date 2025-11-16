550ddddef338c8d95b02c0e83240513c51e767c8
mohamed <mhomedabdo2271@gmail.com>
2025-11-17 01:12:55 +0200

    history section

--- Front-end/js/main.js
+++ Front-end/js/main.js
@@ -1,7 +1,6 @@
 // JavaScript for Dropdown Menu Functionality
 const dropdown = document.querySelector('.dropdown');
 const button = dropdown.querySelector('.dropdown-toggle');
-console.log(button);
 const menu = dropdown.querySelector('.dropdown-menu');
 button.addEventListener('click', function() {
     menu.classList.toggle('smooth-dropdown');
