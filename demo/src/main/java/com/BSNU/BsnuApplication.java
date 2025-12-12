34e5cf984d3b5e26baf018949af073c37e2d5577
mohamed8i69 <109816152+mohamed8i69@users.noreply.github.com>
2025-12-12 21:43:12 +0200

    first step in the back-end
    * nav-view
    
    * history section
    
    * number-section
    
    * the pervios last version for the home page
    
    * contact_test
    
    * start backend
    
    * Initialize README with project details
    
    Added an overview of the Online-Faculty-Staff application, including tech stack, features, and project structure.

---
+++ demo/src/main/java/com/BSNU/BsnuApplication.java
@@ -0,0 +1,14 @@
+package com.BSNU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsnuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsnuApplication.class, args);
+
+	}
+
+}
